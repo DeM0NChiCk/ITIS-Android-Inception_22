0f05faac595b1b9d6a4cc613bd31cd9d8c8665a0
R. Faskhetdinov <cyrax.985@gmail.com>
2022-10-17 14:15:27 +0300

    Diff utils callback added

--- app/src/main/java/com/samples/itis_android_inception_22/presentation/adapters/PlanetsRvAdapter.kt
+++ app/src/main/java/com/samples/itis_android_inception_22/presentation/adapters/PlanetsRvAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.samples.itis_android_inception_22.R
 import com.samples.itis_android_inception_22.databinding.ItemPlanetMainBinding
@@ -29,6 +30,30 @@ class PlanetsRvAdapter(
     }
 
     override fun getItemCount(): Int = planetsList.size
+
+    fun setNewItems(newList: List<PlanetDataModel>) {
+        val diffUtilsCallback = object : DiffUtil.Callback() {
+
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                val oldItemName = planetsList[oldItemPosition].planetName
+                val newItemName = newList[newItemPosition].planetName
+                return oldItemName == newItemName
+            }
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                val oldItemDesc = planetsList[oldItemPosition].descriptionText
+                val newItemDesc = newList[newItemPosition].descriptionText
+                return oldItemDesc == newItemDesc
+            }
+
+            override fun getOldListSize(): Int = planetsList.size
+
+            override fun getNewListSize(): Int = newList.size
+        }
+
+        val diffResult = DiffUtil.calculateDiff(diffUtilsCallback)
+        diffResult.dispatchUpdatesTo(this@PlanetsRvAdapter)
+    }
 
     inner class PlanetViewHolder(private val viewBinding: ItemPlanetMainBinding) :
         RecyclerView.ViewHolder(viewBinding.root) {
